979415f5e67bfb00856f22a8acbdbbac3b1913dc
Kartik Kaul <kartik3322@gmail.com>
2025-06-02 18:28:51 +0530

    added auth

---
+++ middleware.ts
@@ -0,0 +1,30 @@
+import withAuth from "next-auth/middleware";
+import { NextResponse } from "next/server";
+
+export default withAuth(
+    function middleware(){
+        return NextResponse.next()
+    }, {
+        callbacks: {
+            authorized: ({token,req}) => {
+                const {pathname} = req.nextUrl;
+                //allow below routes (auth paths)
+
+                if(pathname.startsWith("/api/auth") || pathname === "/login" || pathname === "/register"){
+                    return true;
+                }
+
+                //public routes
+                if(pathname === "/" || pathname.startsWith("/api/videos")){
+                    return true
+                }
+
+                return !!token  
+            }
+        }
+    }
+)
+
+export const config = {
+    matcher: ["/((?!_next/static|_next/image|favicon.ico|public/).*)"]
+}
