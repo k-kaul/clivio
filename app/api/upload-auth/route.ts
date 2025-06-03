9ed1497cc783d2ddf449375746c7ff5febd5562b
Kartik <kartik3344@gmail.com>
2025-06-03 12:42:28 +0530

    added upload files logic

--- app/api/upload-auth/route.ts
+++ app/api/upload-auth/route.ts
@@ -1,29 +1,29 @@
 import { getServerSession } from "next-auth";
-import { redirect } from "next/navigation";
 import { getUploadAuthParams } from "@imagekit/next/server";
 import { NextResponse } from "next/server";
+import { authOptions } from "@/lib/auth";
 
 export async function GET(){
-    const session = await getServerSession();
+    const session = await getServerSession(authOptions);
     if(!session?.user){
-        return redirect("/login")
+        return NextResponse.redirect("/login")
     }
 
     try {
         const {token, expire, signature} = getUploadAuthParams({
-        privateKey: process.env.IMAGEKIT_PRIVATE_KEY as string,
-        publicKey: process.env.IMAGEKIT_PUBLIC_KEY as string,
-        token: String(Math.random()*1000)
-    })
-    return NextResponse.json({
-        token,
-        expire,
-        signature,
-        publicKey: process.env.IMAGEKIT_PUBLIC_KEY
-    })
+            privateKey: process.env.IMAGEKIT_PRIVATE_KEY as string,
+            publicKey: process.env.IMAGEKIT_PUBLIC_KEY as string,
+            token: Math.random().toString()
+        })
+        return NextResponse.json({
+            token,
+            expire,
+            signature,
+            publicKey: process.env.IMAGEKIT_PUBLIC_KEY
+        })
     } catch (error) {
-      return NextResponse.json({
-        message: "Error in ImageKit Auth File"
-      })  
+        return NextResponse.json({
+            message: "Error in ImageKit Auth File"
+        })  
     }
 }
